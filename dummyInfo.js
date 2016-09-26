3ab0658e58309e9350bd3aed0cf1da2e0e6cba09
jerrymao15 <jerryy.mao@gmail.com>
2016-09-26 16:46:21 -0700

    fixing deleted files

---
+++ dummyInfo.js
@@ -0,0 +1,30 @@
+const dummyData = {
+  matchInfo: {
+    matchName: 'jerrymao',
+    matchID: '123abc',
+    matchPicture: '',
+  },
+  eventInfo: {
+    eventName: 'it will be ok',
+    location: {
+      name: 'my crib',
+      address: '6345 n muscatel ave',
+      city: 'san gabriel',
+      state: 'CA',
+      zip: '91775',
+    },
+    timeAndDate: {
+      startTime: '530',
+      endTime: '640',
+    },
+    peopleGoing: [
+      'just me',
+      'and no one else',
+    ],
+    price: {
+      low: 20,
+      high: 30,
+    },
+    comments: 'bring ur party pants',
+  },
+}
